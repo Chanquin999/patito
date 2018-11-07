068b14d85707f34cdcf09265546155a18ae1442e
Chanquin999 <31196079+Chanquin999@users.noreply.github.com>
2018-11-07 11:27:35 -0600

    Update model/buscar_tabla.php

--- model/buscar_tabla.php
+++ model/buscar_tabla.php
@@ -2,11 +2,20 @@
 require 'connection.php';
 
 $salida = "";
-$query = "SELECT * FROM clientes ORDER BY id";
+$query = "SELECT * FROM clientes ORDER BY codigo";
 
 if (isset($_POST['consulta'])) {
   $q = $mysqli->real_escape_string($_POST['consulta']);
+  $query = "SELECT codigo, nombre, direccion, telefono, nit, servicio FROM clientes WHERE nombre LIKE '%".$q."%'';
 }
+  $resultado = $mysqli->query($query);
 
+  if($resultado->num_rows > 0){
+
+  
+
+  } else {
+
+  }
 
  ?>
